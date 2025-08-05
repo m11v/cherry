9dc61ee21488da8b1f5cb29a9cd25ce5ce4d2ba9
irene.yu <irene.xin.yu@gmail.com>
2025-08-05 15:53:14 -0700

    update

--- whisker/scripts/bump_icon_version.py
+++ whisker/scripts/bump_icon_version.py
@@ -4,7 +4,7 @@ import json
 import glob
 
 CONFIG_PATH = "whisker/configs.toml"
-VERSIONS_DIR = "whisker/versions"
+VERSIONS_DIR = "whisker/icons"
 CHANGED_PATH = "changed/icons.txt"
 
 # 读取当前版本
@@ -25,10 +25,10 @@ with open(CHANGED_PATH, "r") as f:
     changed_files = [line.strip() for line in f.readlines() if line.strip().startswith("whisker/icons/")]
 
 # 构造 changes 数组
-changes = [{"icon": path.replace("../icons/", "")} for path in changed_files]
+changes = [{"icon": path.replace("whisker/icons/", "")} for path in changed_files]
 
 # 创建版本 JSON 文件
-version_file = os.path.join(VERSIONS_DIR, f"{new_version}.json")
+version_file = os.path.join(VERSIONS_DIR, f"version_{new_version}.json")
 with open(version_file, "w") as f:
     json.dump({
         "version": new_version,
